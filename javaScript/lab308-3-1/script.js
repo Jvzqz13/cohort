32a27a57a55fad1eb964be694c6a205a3853c670
Jvzqz13 <142378858+Jvzqz13@users.noreply.github.com>
2023-11-17 15:02:54 -0500

    part 2

--- javaScript/lab308-3-1/script.js
+++ javaScript/lab308-3-1/script.js
@@ -1,4 +1,4 @@
-////Part 1 //// 
+///////////Part 1 ///////////// 
 
 // Loop through all numbers from 1 to 100.
 // If a number is divisible by 3, log “Fizz.”
@@ -6,18 +6,36 @@
 // If a number is divisible by both 3 and 5, log “Fizz Buzz.”
 // If a number is not divisible by either 3 or 5, log the number.
 
-let x = 0; //logs even and odds
-while(x < 100) {
-    if (x % 3 == 0){
-        console.log(`${x} Fizz`);
-    } 
-    if (x % 5 == 0) {
-        console.log(`${x} Buzz`);
+// let x = 0; 
+// while(x < 100) {
+//     if (x % 3 == 0){
+//         console.log(`${x} Fizz`);
+//     } 
+//     if (x % 5 == 0) {
+//         console.log(`${x} Buzz`);
+//     }
+//     if (x % 3 == 0 && x % 5 == 0){
+//         console.log(`${x} Fizz Buzz`);
+//     } else {
+//         console.log(x);
+//     }
+//     x++
+// }
+
+////////////Part 2 /////////////
+
+for ( let n = 4; n <= 40; n++){
+    let isPrime = true; //flag 
+    
+    for(let i = 2; i < n; i++){
+        // i = 2,3,4 
+        if (n % i == 0){
+            isPrime = false;
+            break;
+        }
     }
-    if (x % 3 == 0 && x % 5 == 0){
-        console.log(`${x} Fizz Buzz`);
-    } else {
-        console.log(x);
+    if(isPrime){
+        console.log(`${n} is a prime number`);
     }
-    x++
+
 }
