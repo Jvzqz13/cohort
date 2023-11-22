71a7be88a4f90fc07d47f76af871df83f19eb096
Jvzqz13 <142378858+Jvzqz13@users.noreply.github.com>
2023-11-22 00:35:20 -0500

    finished part 2                     i dont remember

--- javaScript/lab308-1-1/script.js
+++ javaScript/lab308-1-1/script.js
@@ -64,3 +64,53 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
   ((n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2) && 
   !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) && 
   (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
+
+  ///////////////////////// PART 2 /////////////////////////
+  const totalDistance = 1500;
+  const fuelBudget = 175; 
+  const averageCost = 3;
+
+  const mph55 = 30;
+  const mph60 = 28;
+  const mph75 = 23;
+  
+// How many gallons of fuel will you need for the entire trip?
+  let Totalgal55 = totalDistance / mph55;
+  console.log(`At 55MPH, ${Totalgal55} gallons will be needed to travel 1,500 miles.`);
+  let Totalgal60 = totalDistance / mph60;
+  console.log(`At 60MPH, ${Totalgal60} gallons will be needed to travel 1,500 miles.`);
+  let Totalgal75 = totalDistance / mph75;
+  console.log(`At 75MPH, ${Totalgal75} gallons will be needed to travel 1,500 miles.`);
+
+// Will your budget be enough to cover the fuel expense?
+const budgetAt55 = Totalgal55 * averageCost;
+console.log(`Total cost of fuel going at 55MPH will be ${budgetAt55}`);
+
+const budgetAt60 = Totalgal60 * averageCost;
+console.log(`Total cost of fuel going at 60MPH will be ${budgetAt60}`);
+
+const budgetAt75 = Totalgal75 * averageCost;
+console.log(`Total cost of fuel going at 75MPH will be ${budgetAt75}`);
+
+const fuelExpense1 = budgetAt55 < fuelBudget;
+console.log(fuelExpense1);
+
+const fuelExpense2 = budgetAt60 < fuelBudget;
+console.log(fuelExpense2);
+
+const fuelExpense3 = budgetAt75 < fuelBudget;
+console.log(fuelExpense3);
+
+// How long will the trip take, in hours?
+const mph1 = 55;
+const mph2 = 60;
+const mph3 = 75; 
+
+const hoursAt55 = totalDistance / mph1;
+console.log(`At 55MPH, it will take ${hoursAt55} hours to travel 1500miles`);
+
+const hoursAt60 = totalDistance / mph2;
+console.log(`At 60MPH, it will take ${hoursAt60} hours to travel 1500miles`);
+
+const hoursAt75 = totalDistance / mph3;
+console.log(`At 75MPH, it will take ${hoursAt75} hours to travel 1500miles`);
