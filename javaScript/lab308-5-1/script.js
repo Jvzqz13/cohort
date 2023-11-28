dccde77b3e74a3aeb26c944b75c30ec0488b52b1
Jvzqz13 <142378858+Jvzqz13@users.noreply.github.com>
2023-11-28 01:03:50 -0500

    part 1, average of array numbers

--- javaScript/lab308-5-1/script.js
+++ javaScript/lab308-5-1/script.js
@@ -10,6 +10,15 @@ arrayOfNum.forEach( num => {
 console.log(sum);
 
 // Take an array of numbers and return the average.
+
+let averageOfNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let avr = 0;
+
+arrayOfNum.forEach( num => {
+    avr +=num / arrayOfNum.length;
+})
+console.log(avr);
+
 // Take an array of strings and return the longest string.
 // Take an array of strings, and a number and return an array of the strings that are longer than the given number. 
 // For example, stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 3); would return ["hello", "morning"].
