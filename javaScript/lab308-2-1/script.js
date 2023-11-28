cdae65a7ab3c746caee216a05fe0d476c9c32aa8
Jvzqz13 <142378858+Jvzqz13@users.noreply.github.com>
2023-11-27 23:22:24 -0500

    finsihed part 3

--- javaScript/lab308-2-1/script.js
+++ javaScript/lab308-2-1/script.js
@@ -3,11 +3,11 @@
 //area = pi * radius * radius;
 const pi = 3.1415;
 const radius = 5;
-const areaOfContainer = pi *radius *radius; //area of plant container 
+const areaOfContainer = pi *radius *radius; //max capacity 
 
 let weeks = 2;
-const minSpace = .8; 
-const numOfPlants = 20;
+const minSpace = .8;
+let numOfPlants = 20;
 let doubleWeekly = numOfPlants * 2 ** weeks;
 
 let currentCap = minSpace * doubleWeekly; 
@@ -22,9 +22,21 @@ if (capPercent >= 0.8) {
 }
 
 // Part 2 // 
+numOfPlants = 100;
+weeks = 10;
+let newTotalAreaNeeded = numOfPlants * weeks;
+console.log(newTotalAreaNeeded);
 
+//part 3 
 
-
-
-
-
+try {
+    if (capPercent >= 0.8) {
+      throw new Error("The space required for the plants exceeds the available space.");
+    } else if (capPercent <= 0.8 && capPercent > 0.5) {
+      console.log("We're good");
+    } else {
+      console.log("Plant more");
+    }
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
