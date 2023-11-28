7619a024a5797745cd6653a75f5307dc9a0362c3
Jvzqz13 <142378858+Jvzqz13@users.noreply.github.com>
2023-11-28 01:41:29 -0500

    Part 1, longest string

--- javaScript/lab308-5-1/script.js
+++ javaScript/lab308-5-1/script.js
@@ -20,6 +20,23 @@ arrayOfNum.forEach( num => {
 console.log(avr);
 
 // Take an array of strings and return the longest string.
+let stringArray = ["hello","world","whatsup","dude","hi"];
+
+function longest(arr){
+
+    let longestString = arr[0]; 
+
+   for(let i = 0; i < arr.length; i++){
+
+    if(arr[i].length > longestString.length){
+        longestString = arr[i];
+        return longestString;
+    }
+   }
+}
+let longestStr = longest(stringArray);
+console.log(longestStr);
+
 // Take an array of strings, and a number and return an array of the strings that are longer than the given number. 
 // For example, stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 3); would return ["hello", "morning"].
 // Take a number, n, and print every number between 1 and n without using loops. Use recursion.
