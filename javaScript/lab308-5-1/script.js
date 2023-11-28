1a7e58b84e3b07a32749f42e81f59a7f1d85dd58
Jvzqz13 <142378858+Jvzqz13@users.noreply.github.com>
2023-11-28 00:58:50 -0500

    part 1 done

---
+++ javaScript/lab308-5-1/script.js
@@ -0,0 +1,16 @@
+//////////////////// Part 1 ////////////////////
+
+// Take an array of numbers and return the sum.
+let arrayOfNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let sum = 0;
+
+arrayOfNum.forEach( num => {
+    sum +=num
+})
+console.log(sum);
+
+// Take an array of numbers and return the average.
+// Take an array of strings and return the longest string.
+// Take an array of strings, and a number and return an array of the strings that are longer than the given number. 
+// For example, stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 3); would return ["hello", "morning"].
+// Take a number, n, and print every number between 1 and n without using loops. Use recursion.
