c841612202a110261283442c2070d615fbcdae10
Jvzqz13 <142378858+Jvzqz13@users.noreply.github.com>
2024-01-09 10:13:32 -0500

    logging middleware

--- rest-api/server.js
+++ rest-api/server.js
@@ -9,6 +9,22 @@ const posts = require("./data/posts");
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+// New logging middleware to help us keep track of
+// requests during testing!
+app.use((req, res, next) => {
+  const time = new Date();
+
+  console.log(
+    `-----
+${time.toLocaleTimeString()}: Received a ${req.method} request to ${req.url}.`
+  );
+  if (Object.keys(req.body).length > 0) {
+    console.log("Containing the data:");
+    console.log(`${JSON.stringify(req.body)}`);
+  }
+  next();
+});
+
 
 
 // GET All Users 
